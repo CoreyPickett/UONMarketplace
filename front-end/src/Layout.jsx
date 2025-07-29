efe71a9e15b4a9a1d14e1d8d80b60befdd27a435
Jaden McSkimming <jadenwmcskimming@gmail.com>
2025-07-29 14:56:53 +1000

    Added a phone number section for registartion page with proper formatting

--- front-end/src/Layout.jsx
+++ front-end/src/Layout.jsx
@@ -1,12 +1,18 @@
 //Layout of application
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
+import { use } from 'react';
 
 
 export default function Layout() {
+  const location = useLocation();
+
+  const hiddenHeader =['/login', '/registration', '/forgotpswd']
+  const hideHeader = hiddenHeader.includes(location.pathname);
+
   return (
     <>
-    <Header />
+    {!hideHeader && <Header />}
     <Outlet />
     </>
   );
