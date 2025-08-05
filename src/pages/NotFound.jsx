df9587bf03bee501a212a3dba460238a023035f3
CoreyPickett <pickettcorey23@gmail.com>
2025-08-05 16:51:22 +1000

    Quick fix for localhost not found
    Moved the index.html file into the front-end folder and deleted 'src' and 'public' folders outside of front-end and back-end

--- src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-//Custome error page, placeholder information
-export default function NotFound() {
-  return (
-    <>
-    <h1>Page Not Found</h1>
-    <p>The link you followed to get here must be broken...</p>
-    </>
-  );
-}
